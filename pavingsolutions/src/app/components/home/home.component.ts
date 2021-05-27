7d119cf908847c3eb20df60ad85b2518ecc9316e
Botond Kulcsar <bkulcsar@gmx.com>
2021-05-27 11:09:24 +0300

    added bs cards

--- pavingsolutions/src/app/components/home/home.component.ts
+++ pavingsolutions/src/app/components/home/home.component.ts
@@ -7,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  services: any[] = [
+    {
+      title: 'Paving',
+      description: 'Fully professional fitting service.',
+      imgUrl : '../../../assets/services/paving.jpeg'
+    },
+    {
+      title: 'Tools and Machinery Hire',
+      description: 'We offer you a wide range of rental equipment.',
+      imgUrl : '../../../assets/services/tools.jpeg'
+    },
+    {
+      title: 'Maintenance services',
+      description: 'A very important maintaining quality is the rendering of the look.',
+      imgUrl : '../../../assets/services/maintenance3.jpeg'
+    },
+    {
+      title: 'Counseling',
+      description: 'Advice on chooising products,  designing-viewing 3D layout and calculating material requirements.',
+      imgUrl : '../../../assets/services/counseling.jpeg'
+    },
+
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
